2e8c592d13d395d65ec2671444520a2c028a3d7f
Max Lichter <iammaxlichter@gmail.com>
2024-10-30 15:30:53 -0500

    done with fe

---
+++ JS/airportJS/airport.js
@@ -0,0 +1,14 @@
+// navbar.js
+
+window.onload = function () {
+    function updateClock() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        const timeString = `${hours}:${minutes}:${seconds}`;
+        document.getElementById("current-time").textContent = timeString;
+    }
+    setInterval(updateClock, 1000);
+    updateClock();
+};
