1077273ba2924114c6b3efadc0a2973d24436ac2
Thejaswin <thejaskumar100@gmail.com>
2024-11-14 15:39:30 -0600

    Routing POC

--- FE/webapp/app/page.tsx
+++ FE/webapp/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -16,16 +15,7 @@ export default function Home() {
         </h1>
         {/* <br></br> */}
 
-        
-
-        <div className="textInputWrapper">
-          <p>
-            What is your name?
-          </p>
-
-          <input placeholder="Name" type="text" className="textInput">
-          </input>
-        </div>
+      
 
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
@@ -41,13 +31,13 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="./create"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Deploy now
+            Make a new ticket
           </a>
-          
+
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -56,6 +46,8 @@ export default function Home() {
           >
             Read our docs
           </a>
+
+
         </div>
       </main>
     </div>
---
+++ FE/webapp/app/search/page.tsx
@@ -0,0 +1,32 @@
+export default function Home() {
+    return (
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          
+          <h1 className="text-3xl sm:text-4xl font-bold text-center sm:text-left">
+            Create a New Ticket
+            <a
+              className="text-primary hover:underline dark:text-primary-dark"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            </a>
+          </h1>
+          {/* <br></br> */}
+  
+          
+  
+          <div className="textInputWrapper">
+            <p>
+              What is your name?
+            </p>
+  
+            <input placeholder="Name" type="text" className="textInput">
+            </input>
+          </div>
+  
+        </main>
+      </div>
+    );
+  }
+  
